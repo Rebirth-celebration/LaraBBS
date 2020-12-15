204c1b22d3f1010f0b6c23b6988da7551512397e
Rebirth-celebration <786742044@qq.com>
2020-12-15 01:12:16 +0000

    上传头像

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -25,8 +25,8 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' .
-                Auth::id(),
+            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,'
+                . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
         ];
