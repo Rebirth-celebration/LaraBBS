35400a77004adc4fd9a7d7411c7babbe7f5f8de5
Rebirth-celebration <786742044@qq.com>
2020-12-16 05:17:15 +0000

    用户话题列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -52,14 +52,19 @@
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('users.show',Auth::user()->id) }}">
-                                个人中心
+                                <i class="fa fa-user mr-2">
+                                    个人中心
+                                </i>
                             </a>
                             <a class="dropdown-item" href="{{ route('users.edit',Auth::id()) }}">
-                                编辑资料
+                                <i class="fa fa-edit mr-2">
+                                    编辑资料
+                                </i>
                             </a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" id="logout" href="#">
-                                <form action="{{ route('logout') }}" method="POST">
+                                <form action="{{ route('logout') }}" method="POST"
+                                onsubmit="return confirm('您确定要退出吗？');">
                                     {{ csrf_field() }}
                                     <button class="btn btn-block btn-danger" type="submit"
                                             name="button">退出
