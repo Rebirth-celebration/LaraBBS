f75d60eeeed94e9fb7ba35fa14d241505676e347
Rebirth-celebration <786742044@qq.com>
2020-12-18 00:59:19 +0000

    回复数据

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(TopicsTableSeeder::class);
+		$this->call(RepliesTableSeeder::class);
+
     }
 }
